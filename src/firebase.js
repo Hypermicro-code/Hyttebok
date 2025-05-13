9d89978944886694a2f32836c3a55608a7bf819f
Hypermicro-code <tony2mat@gmail.com>
2025-05-13 13:09:40 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDQNEjM_6rQvKaeMbcIyd6hGmIynqcW7wQ",
-    authDomain: "hyttebok-bca8a.firebaseapp.com",
-    projectId: "hyttebok-bca8a",
-    storageBucket: "hyttebok-bca8a.appspot.com",
-    messagingSenderId: "XXXXXX",
-    appId: "XXXXXX"
+  apiKey: "DIN_API_KEY",
+  authDomain: "hyttebok-bca8a.firebaseapp.com",
+  projectId: "hyttebok-bca8a",
+  storageBucket: "hyttebok-bca8a.firebasestorage.app", // KORREKT bucket
+  messagingSenderId: "DIN_MESSAGING_ID",
+  appId: "DIN_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
