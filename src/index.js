efad1a14562ab6033a1dc23e349251072472bc4d
Hypermicro-code <tony2mat@gmail.com>
2025-05-13 13:13:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Hvis du bruker språkstøtte, kan du legge inn global config her senere
-// For nå, kun App
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
