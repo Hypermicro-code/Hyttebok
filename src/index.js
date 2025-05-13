a29f229d73473db035fae725b40abc49722bcb0c
Hypermicro-code <tony2mat@gmail.com>
2025-05-13 12:37:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// Hvis du bruker språkstøtte, kan du legge inn global config her senere
+// For nå, kun App
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
