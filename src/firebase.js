fcc21350bedd3e53af6d72fe63a77d101b628bf0
Hypermicro-code <tony2mat@gmail.com>
2025-05-13 08:15:04 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQNEjM_6rQvKaeMbcIyd6hGmIynqcW7wQ",
-  authDomain: "hyttebok-bca8a.firebaseapp.com",
-  projectId: "hyttebok-bca8a",
-  storageBucket: "hyttebok-bca8a.firebasestorage.app",
-  messagingSenderId: "170937664052",
-  appId: "1:170937664052:web:1837c49267453c25d6c061"
+    apiKey: "DIN_API_KEY",
+    authDomain: "DITT_PROSJEKT.firebaseapp.com",
+    projectId: "DITT_PROSJEKT",
+    storageBucket: "DITT_PROSJEKT.appspot.com",
+    messagingSenderId: "XXXXXX",
+    appId: "XXXXXX"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { db };
+export { db, storage };
